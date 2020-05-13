191a1bfc4168b8698fdbb455dd141e4633e78597
mahulst <michel@voorkanter.com>
2020-05-13 15:59:27 +0200

    --wip-- [skip ci]

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,17 @@
-import { Color, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from 'three';
-import { Brick } from './brick';
+import {
+  BoxBufferGeometry,
+  BufferAttribute,
+  BufferGeometry,
+  Color,
+  CylinderBufferGeometry,
+  Group, LineBasicMaterial, LineSegments,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  Vector3,
+  WebGLRenderer
+} from 'three';
 
 export class App {
   private readonly scene = new Scene();
@@ -9,13 +21,14 @@ export class App {
     canvas: document.getElementById('main-canvas') as HTMLCanvasElement,
   });
 
-  private brick: Brick;
+  private brick: Group;
 
   constructor() {
-    this.brick = new Brick(100, new Color('rgb(255,0,0)'));
+    this.brick = createAxis([[1, 0, 0], [0, 1, 0], [0, 0, 1]], 2, 2);
+
     this.scene.add(this.brick);
 
-    this.camera.position.set(200, 200, 200);
+    this.camera.position.set(20, 20, 20);
     this.camera.lookAt(new Vector3(0, 0, 0));
 
     this.renderer.setSize(innerWidth, innerHeight);
@@ -38,3 +51,65 @@ export class App {
     this.brick.rotateY(0.03);
   }
 }
+
+export const createAxis = (
+                             allAxesVectors: [number,number, number][],
+                             axisIndex: number,
+                             size: number,
+                             opacity = 1,
+                             alwaysOnTop = true,
+                           ) => {
+  const arrowGeometry = new CylinderBufferGeometry(0, 0.1 * size, size * 0.3, 12, 1, false);
+
+  arrowGeometry.rotateX(Math.PI / 2)
+  const group = new Group();
+  const mesh = new Mesh(
+    arrowGeometry,
+    new MeshBasicMaterial({
+      depthTest: false,
+      depthWrite: !alwaysOnTop,
+      transparent: true,
+      opacity,
+      color: new Color(255,0,0),
+    }),
+  );
+  const axisVector = allAxesVectors[axisIndex];
+
+  mesh.up.set(0, 1, 0);
+
+  var geometry = new BoxBufferGeometry( 0.1, 0.1, 0.1 );
+  var material = new MeshBasicMaterial( {color: 0x00ff00} );
+  var cube = new Mesh( geometry, material );
+
+  const point = new Vector3(0, 1, 1)
+  cube.position.set(point.x,point.y,point.z)
+  group.add(cube)
+  mesh.lookAt(new Vector3(...axisVector));
+
+  cube.position.add(new Vector3(...axisVector));
+  mesh.position.set(...axisVector).multiplyScalar(size * 0.85);
+  mesh.updateMatrix();
+  const lineGeometry = new BufferGeometry();
+  lineGeometry.setAttribute(
+    'position',
+    new BufferAttribute(
+      Float32Array.from([0, 0, 0, ...new Vector3(...axisVector).multiplyScalar(size * 0.7).toArray()]),
+      3,
+    ),
+  );
+  const line = new LineSegments(
+    lineGeometry,
+    new LineBasicMaterial({
+      color: new Color(255,0,0),
+      linewidth: 2,
+      transparent: true,
+      depthTest: false,
+      depthWrite: !alwaysOnTop,
+      opacity,
+    }),
+  );
+
+  group.add(line);
+  group.add(mesh);
+  return group;
+};
